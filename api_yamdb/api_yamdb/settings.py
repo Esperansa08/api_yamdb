fbab2bea85ce3941ca72acef615472f39ba97ce6
Александр Реутов <sreutov2008@yandex.ru>
2023-02-23 16:57:19 +0700

    убрал из settings генерацию ключа

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -4,8 +4,6 @@ from datetime import timedelta
 
 from dotenv import load_dotenv
 
-from django.core.management.utils import get_random_secret_key
-
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
