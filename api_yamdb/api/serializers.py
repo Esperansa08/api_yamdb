9ec651d0691443f2de4fb06f071fc86302971d20
Александр Реутов <sreutov2008@yandex.ru>
2023-02-26 18:48:43 +0700

    Доделал UsersViews,permissions, serializers и чуть чуть модель User

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -4,37 +4,6 @@ from rest_framework.validators import UniqueValidator
 
 from users.models import User
 
-
-class SignupSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(
-        validators=[
-            UniqueValidator(queryset=User.objects.all())
-        ]
-    )
-    email = serializers.EmailField(
-        validators=[
-            UniqueValidator(queryset=User.objects.all())
-        ]
-    )
-
-    def validate_username(self, value):
-        if value.lower() == 'me':
-            raise serializers.ValidationError('Имя пользователя "me" не допустимо')
-        return value
-
-    class Meta:
-        fields = ('username', 'email')
-        model = User
-
-
-class TokenSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    confirmation_code = serializers.CharField()
-
-    class Meta:
-        model = User
-        fields = ('username', 'confirmation_code')
-
 import datetime as dt
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
@@ -102,3 +71,34 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = '__all__'
         lookup_field = 'slug'
 
+class SignupSerializer(serializers.Serializer):
+    username = serializers.RegexField(
+        r'^[\w.@+-]+$',
+        max_length=150,
+        required=True
+    )
+    email = serializers.EmailField(required=True, max_length=254)
+
+    def validate_username(self, value):
+        if value == 'me':
+            raise serializers.ValidationError(
+                'Имя пользователя "me" не допустимо!'
+            )
+        return value
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = ['username', 'email', 'first_name',
+                  'last_name', 'bio', 'role']
+        model = User
+        
+
+class TokenSerializer(serializers.Serializer):
+    username = serializers.RegexField(
+        r'^[\w.@+-]+$',
+        max_length=150,
+        required=True
+    )
+    confirmation_code = serializers.CharField(required=True)
--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,15 +1,17 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from api.views import (token, signup, TitleViewSet, GenreViewSet, CategoryViewSet)
+from api.views import (signup, token, users_me, TitleViewSet, GenreViewSet, CategoryViewSet, UserViewSet)
 
 v1_router = routers.DefaultRouter()
 v1_router.register('titles', TitleViewSet)
 v1_router.register('genres', GenreViewSet)
 v1_router.register('categories', CategoryViewSet)
+v1_router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
     path('v1/auth/signup/', signup, name='signup'),
     path('v1/auth/token/', token, name='token'),
+    path('v1/users/me/', users_me),
     path('v1/', include(v1_router.urls)),
 ]
