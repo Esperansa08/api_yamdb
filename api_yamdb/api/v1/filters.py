1d7525646f98a709ee199e15c1254098d724e021
Esperansa08 <113490138+Esperansa08@users.noreply.github.com>
2023-03-01 23:16:48 +0700

    Merge branch 'develop' of https://github.com/Esperansa08/api_yamdb into refactoring/validation/del_forms (#28)

--- api_yamdb/api/v1/filters.py
+++ api_yamdb/api/v1/filters.py
@@ -1,6 +1,5 @@
-from django_filters.rest_framework import BaseInFilter, CharFilter, FilterSet
-
 from reviews.models import Title
+from django_filters.rest_framework import BaseInFilter, CharFilter, FilterSet
 
 
 class CharFilterInFilter(BaseInFilter, CharFilter):
@@ -8,15 +7,11 @@ class CharFilterInFilter(BaseInFilter, CharFilter):
 
 
 class TitleFilter(FilterSet):
-    genre = CharFilterInFilter(
-        field_name='genre__slug',
-        lookup_expr='in',
-    )
-    category = CharFilterInFilter(
-        field_name='category__slug',
-        lookup_expr='in',
-    )
-
+    genre = CharFilterInFilter(field_name='genre__slug', )
+    category = CharFilterInFilter(field_name='category__slug',)
+    name = CharFilterInFilter(
+        field_name='name',
+        lookup_expr='in',)
     class Meta:
         model = Title
         fields = ('genre', 'category', 'year', 'name',)
--- api_yamdb/api/v1/serializers.py
+++ api_yamdb/api/v1/serializers.py
@@ -1,3 +1,4 @@
+import datetime as dt
 from django.contrib.auth import get_user_model
 from django.core import validators
 from django.db.models import Avg
@@ -6,7 +7,7 @@ from rest_framework import serializers, viewsets
 from rest_framework.validators import UniqueValidator
 
 from reviews.models import Category, Comment, Genre, Review, Title
-from .exceptions import BadRating
+from .exceptions import BadRating, IncorrectTitleInYear
 
 User = get_user_model()
 
@@ -39,8 +40,8 @@ class TitleSerializerRead(serializers.ModelSerializer):
 
     class Meta:
         model = Title
-        fields = ['id', 'name', 'year', 'rating', 'description', 'genre',
-                  'category']
+        fields = ('id', 'name', 'year', 'rating', 'description', 'genre',
+                  'category')
 
 
 class TitleSerializerWrite(serializers.ModelSerializer):
@@ -54,32 +55,15 @@ class TitleSerializerWrite(serializers.ModelSerializer):
 
     class Meta:
         model = Title
-        fields = ['id', 'name', 'year', 'rating', 'description', 'genre',
-                  'category']
+        fields = ('id', 'name', 'year', 'rating', 'description', 'genre',
+                  'category')
 
-    def to_representation(self, instance):
-        request = self.context.get('request')
-        context = {'request': request}
-        return TitleSerializerRead(instance, context=context).data
-
-
-class TitleViewSet(viewsets.ModelViewSet):
-    queryset = Title.objects.all()
-
-    def get_serializer_class(self):
-        if self.request.method == 'GET':
-            return TitleSerializerRead
-        return TitleSerializerWrite
-
-    def perform_create(self, serializer):
-        category_slug = self.request.data['category']
-        genre = self.request.data['genre']
-        category = get_object_or_404(Category, slug=category_slug)
-        genres = []
-        for item in genre:
-            genres.append(get_object_or_404(Genre, slug=item))
-        serializer.save(category=category, genres=genres)
-
+    def validate_year(self, value):
+        year_now = dt.date.today().year
+        print(year_now)
+        if value > year_now:
+            raise IncorrectTitleInYear('Передано некорректное значение года')
+        return value 
 
 class ReviewSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
@@ -117,8 +101,8 @@ class SignupSerializer(serializers.Serializer):
 class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
-        fields = ['username', 'email', 'first_name',
-                  'last_name', 'bio', 'role']
+        fields = ('username', 'email', 'first_name',
+                  'last_name', 'bio', 'role')
         model = User
 
 
